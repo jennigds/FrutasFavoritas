3746fb3848f1ac62a9b9b5ab952219a64129c75c
pão <jennifergds89@gmail.com>
2023-03-03 20:20:31 -0300

    Frutas Preferidas

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Frutas Favoritas");
+Console.WriteLine("1. Nectarina");
+Console.WriteLine("2. Manga");
+Console.WriteLine("3. Goiaba");
+Console.WriteLine("4. Morango");
+Console.WriteLine("5. Bergamota");
